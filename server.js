7f811796b8bdf90571f4cce5cf8f81ca48114271
portiaportia <portiaplante@gmail.com>
2019-11-20 15:06:58 -0500

    updated to have add

--- server.js
+++ server.js
@@ -9,8 +9,67 @@ mongoose.connect('mongodb://localhost/recipes', {useUnifiedTopology:true, useNew
 .then(()=> console.log("Connected to mongodb..."))
 .catch((err => console.error("could not connect ot mongodb...", err)));
 
+const recipeSchema = new mongoose.Schema({
+    title:String,
+    author:String,
+    rating:Number,
+    ingredients:[String],
+    directions:[String]
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+async function createRecipe(recipe){
+    const result = await recipe.save();
+    console.log(result);
+}
 
 /*
+const chocCookie = new Recipe({
+    title:"Chocolate Chip Cookies",
+    author:"Sam Young",
+    rating:3.4,
+    ingredients:["flour","sugar","butter","milk","eggs","chips"],
+    directions:["mix dry ingredients", "mix wet ingredients", "mix everything", "put on cookie sheet", "bake"]
+});
+
+createRecipe(chocCookie);
+*/
+
+function validateRecipe(recipe){
+    const schema = {
+        title:Joi.string().min(3).required(),
+        author:Joi.string(),
+        rating:Joi.number(),
+        ingredients:Joi.allow(),
+        directions:Joi.allow()
+    };
+
+    return Joi.validate(recipe, schema);
+}
+
+app.post('/api/recipes', (req,res)=>{
+    const result = validateRecipe(req.body);
+
+    if(result.error){
+        res.status(400).send(result.err.details[0].message);
+        return;
+    }
+
+    const recipe = new Recipe({
+        title:req.body.title,
+        author:req.body.author,
+        rating:Number(req.body.rating),
+        ingredients:req.body.ingredients,
+        directions:req.body.directions
+    });
+
+    createRecipe(recipe);
+    res.send(recipe);
+
+});
+
+
 //render our html page
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
@@ -19,4 +78,4 @@ app.get('/',(req,res)=>{
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
-});*/
+});
